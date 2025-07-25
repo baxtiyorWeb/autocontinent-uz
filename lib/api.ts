218a471927807a681b16af18108580619b015ec4
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-07-25 15:28:42 +0500

    fixed

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://autocontinent-uz.vercel.app/api",
+  baseURL: "https://avtokontinent-uz.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
